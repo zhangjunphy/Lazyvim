1e8f28cac700deefc9fdaf642d3167e89db33f29
linchuan <clin@siorigin.com>
2025-01-08 23:37:04 +0800

    add some plugins

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,112 @@
+local function format_hunks()
+  local ignore_filetypes = { "lua" }
+  if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+    vim.notify("range formatting for " .. vim.bo.filetype .. " not working properly.")
+    return
+  end
+
+  local hunks = require("gitsigns").get_hunks()
+  if hunks == nil then
+    return
+  end
+
+  local format = require("conform").format
+
+  local function format_range()
+    if next(hunks) == nil then
+      vim.notify("done formatting git hunks", "info", { title = "formatting" })
+      return
+    end
+    local hunk = nil
+    while next(hunks) ~= nil and (hunk == nil or hunk.type == "delete") do
+      hunk = table.remove(hunks)
+    end
+
+    if hunk ~= nil and hunk.type ~= "delete" then
+      local start = hunk.added.start
+      local last = start + hunk.added.count
+      -- nvim_buf_get_lines uses zero-based indexing -> subtract from last
+      local last_hunk_line = vim.api.nvim_buf_get_lines(0, last - 2, last - 1, true)[1]
+      local range = { start = { start, 0 }, ["end"] = { last - 1, last_hunk_line:len() } }
+      format({ range = range, async = true, lsp_fallback = true }, function()
+        vim.defer_fn(function()
+          format_range()
+        end, 1)
+      end)
+    end
+  end
+
+  format_range()
+end
+
+return {
+  "stevearc/conform.nvim",
+  dependencies = { "mason.nvim" },
+  lazy = true,
+  cmd = "ConformInfo",
+  keys = {
+    {
+      "<leader>cF",
+      function()
+        require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
+      end,
+      mode = { "n", "v" },
+      desc = "Format Injected Langs",
+    },
+  },
+  init = function()
+    -- Install the conform formatter on VeryLazy
+    LazyVim.on_very_lazy(function()
+      LazyVim.format.register({
+        name = "conform.nvim",
+        priority = 100,
+        primary = true,
+        format = function(buf)
+          require("conform").format({ bufnr = buf })
+        end,
+        sources = function(buf)
+          local ret = require("conform").list_formatters(buf)
+          ---@param v conform.FormatterInfo
+          return vim.tbl_map(function(v)
+            return v.name
+          end, ret)
+        end,
+      })
+    end)
+  end,
+  opts = {
+    default_format_opts = {
+      timeout_ms = 3000,
+      async = false, -- not recommended to change
+      quiet = false, -- not recommended to change
+      lsp_format = "fallback", -- not recommended to change
+    },
+    formatters_by_ft = {
+      lua = { "stylua" },
+      fish = { "fish_indent" },
+      sh = { "shfmt" },
+      cpp = { "clang-format" },
+    },
+    format_on_save = function()
+      -- Prefer to format git hunks instead of the entire file
+      format_hunks()
+    end,
+    -- The options you set here will be merged with the builtin formatters.
+    -- You can also define any custom formatters here.
+    ---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
+    formatters = {
+      injected = { options = { ignore_errors = true } },
+      -- # Example of using dprint only when a dprint.json file is present
+      -- dprint = {
+      --   condition = function(ctx)
+      --     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
+      --   end,
+      -- },
+      --
+      -- # Example of using shfmt with extra args
+      -- shfmt = {
+      --   prepend_args = { "-i", "2", "-ci" },
+      -- },
+    },
+  },
+}
