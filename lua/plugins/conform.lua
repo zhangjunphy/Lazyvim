94c58ca6f578e83de5b45ffb315a8992b4dddfe2
linchuan <clin@siorigin.com>
2025-01-22 17:14:21 +0800

    Reimplement auto format

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,42 +1,25 @@
 local function format_hunks(bufnr)
-  local ignore_filetypes = { "lua" }
-  if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
-    vim.notify("range formatting for " .. vim.bo.filetype .. " not working properly.")
-    return
-  end
+  local format = require("conform").format
 
   local hunks = require("gitsigns").get_hunks(bufnr)
   if hunks == nil then
     return
   end
-
-  local format = require("conform").format
-
-  local function format_range()
-    if next(hunks) == nil then
-      vim.notify("done formatting git hunks", "info", { title = "formatting" })
-      return
-    end
-    local hunk = nil
-    while next(hunks) ~= nil and (hunk == nil or hunk.type == "delete") do
-      hunk = table.remove(hunks)
+  for _, hunk in ipairs(hunks) do
+    if hunk.type == "delete" then
+      goto continue
     end
 
-    if hunk ~= nil and hunk.type ~= "delete" then
-      local start = hunk.added.start
-      local last = start + hunk.added.count
-      -- nvim_buf_get_lines uses zero-based indexing -> subtract from last
-      local last_hunk_line = vim.api.nvim_buf_get_lines(0, last - 2, last - 1, true)[1]
-      local range = { start = { start, 0 }, ["end"] = { last - 1, last_hunk_line:len() } }
-      format({ range = range, async = true, lsp_fallback = true, bufnr = bufnr }, function()
-        vim.defer_fn(function()
-          format_range()
-        end, 1)
-      end)
-    end
+    local start = hunk.added.start
+    local last = start + hunk.added.count
+    -- nvim_buf_get_lines uses zero-based indexing -> subtract from last
+    local last_hunk_line = vim.api.nvim_buf_get_lines(bufnr, last - 2, last - 1, true)[1]
+    local range = { start = { start, 0 }, ["end"] = { last - 1, last_hunk_line:len() } }
+    format({ range = range, async = false, lsp_fallback = true, bufnr = bufnr })
+    ::continue::
   end
 
-  format_range()
+  vim.notify("done formatting git hunks", vim.log.levels.TRACE, { title = "formatting" })
 end
 
 return {
@@ -45,6 +28,14 @@ return {
   lazy = true,
   cmd = "ConformInfo",
   keys = {
+    {
+      "<leader>cf",
+      function()
+  require("conform").format({ timeout_ms = 3000 })
+      end,
+      mode = { "n", "v" },
+      desc = "Format",
+    },
     {
       "<leader>cF",
       function()
@@ -80,6 +71,7 @@ return {
       fish = { "fish_indent" },
       sh = { "shfmt" },
       cpp = { "clang-format" },
+      tablegen = { "clang-format" },
     },
   },
 }
