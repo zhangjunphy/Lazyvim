b999bd1e82448d9b4b9ce00c5caa6fa4675182af
linchuan <clin@siorigin.com>
2025-08-27 10:31:33 +0800

    change colorscheme

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,7 +9,6 @@ return {
     },
     {
       'nvim-telescope/telescope-fzf-native.nvim',
-      build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 && cmake --build build --config Release'
     }
   },
   config = function()
