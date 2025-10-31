82abd47a2a78c5c3244ffaebe7e43a4afaa5b2ab
linchuan <clin@siorigin.com>
2025-10-31 11:46:42 +0800

    replace neogit with fugitive

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,20 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+local readOnlyPaths = {"$HOME/.local", "$HOME/.elan", "$HOME/.rustup", "$HOME/.npm", "$HOME/.cargo"};
+local readOnlyAbsPath = {}
+for _, p in ipairs(readOnlyPaths) do
+  local path = vim.fn.expand(p)
+  if vim.fn.isdirectory(p) then
+    path = path .. "/*"
+  end
+  readOnlyAbsPath[#readOnlyAbsPath+1] = path
+end
+vim.api.nvim_create_autocmd("BufReadPost", {
+  pattern = readOnlyAbsPath,
+  callback = function ()
+    vim.bo.readonly = true
+    vim.bo.modifiable = false
+  end,
+})
--- lua/plugins/typst.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'chomosuke/typst-preview.nvim',
-  lazy = false,
-  version = '1.*',
-  opts = {
-    port = 3000,
-    debug = true,
-  },
-}
