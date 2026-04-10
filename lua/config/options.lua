464ae06037f438e1747ef46bb7bbb780a4ce9bd7
linchuan <clin@siorigin.com>
2026-04-10 17:54:03 +0800

    set autochdir, update colorscheme

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+# chdir based on current file
+vim.opt.autochdir = true
+
 vim.g.autoformat = false
 vim.g.snacks_animate = false
 
